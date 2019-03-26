93024f787f546bcfddf1fd6203f76cce56640378
Lansfeust <lansfeust@hotmail.fr>
2019-03-26 07:31:01 +0100

    bdd + racine opérationnel

--- fonction/bdd.php
+++ fonction/bdd.php
@@ -1,7 +1,8 @@
 <?php
 
-
-
-
-
+//	$bdd =new PDO('mysql:host=localhost;dbname=___flashCard___','root','passe') ; 
+//
+//Hostname: 127.0.0.1, username: votre_utilisateur, password: votre_mdp, db: sakila
+//
+$bdd = new mysqli('localhost', 'phpmyadmin', '123456', 'flashCard');
 ?>
