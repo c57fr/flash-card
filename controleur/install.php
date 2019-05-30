be69c8b6777fb1c866d7f12ecf7086bd2d386343
lansfeust <lansfeust@hotmail.fr>
2019-05-30 13:52:31 +0200

    ajout de la création du la table grace au fichier install

---
+++ controleur/install.php
@@ -0,0 +1,14 @@
+<?php
+
+$ite = $_COOKIE["serveur"] ;
+$racine = $_COOKIE["racine"] ;
+
+
+	include_once("$racine/Vue/head.html") ;
+	include_once("$racine/Vue/menu.html") ;
+	include_once("$racine/fonction/creationTable.php");
+	include_once("$racine/Vue/Footer.html");
+
+
+
+?>
---
+++ fonction/creationTable.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once("$racine/fonction/bdd.php");
+
+
+$creationFamille = "CREATE TABLE IF NOT EXISTS famille (
+idFamille SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+nom TEXT(550) NOT NULL
+) " ;
+ 
+$creationCards = "CREATE TABLE IF NOT EXISTS cards (
+idCards SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+Question TEXT(550) NOT NULL ,
+nom TEXT(550) NOT NULL ,
+Reponse TEXT(550) NOT NULL ,
+IdFamille SMALLINT UNSIGNED NOT NULL ,
+DateDeVue DATE NOT NULL
+) " ;
+
+
+$count = $bdd->exec($creationFamille);
+$count = $bdd->exec($creationCards);
+
+?>
+<h1>création de la table</h1>
