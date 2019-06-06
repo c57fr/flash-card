4f859bf3045de5dcd5f3ed1e59e1d0a5f072b74e
moi <moi@moi.moi>
2019-06-06 22:48:04 +0200

    teste a effacer

--- uuu.php
+++ uuu.php
@@ -1,31 +1,16 @@
 < > <?php
 
-        $ite = $_COOKIE["serveur"] ;
-        $racine = $_COOKIE["racine"] ;
-
-        include_once("$racine/fonction/bdd.php") ;
-
-        $reponse = $bdd->query('
-        SELECT * FROM cards WHERE niveau != 0 AND dateDeVue < NOW() ORDER BY RAND() LIMIT 1;
-        ');
-        $donnees = $reponse->fetch() ;
-        $_SESSION['ID'] = $donnees['ID'] ;
-        $_SESSION['niveau'] = $donnees['niveau'] ;
+        function rechercheFmille($id)
+        {
+            include_once("$racine/fonction/bdd.php") ;
         
+            $req = $bdd->prepare('SELECT * FROM famille WHERE  famille= ? ) VALUES ( :famille ) ';
+            
+            $req->execute(array(
+            'famille' => $id 
+            ));
+            
+            $donnees = $reponse->fetch() ;
 
-
-
-
-$sth = $bdd->prepare("SELECT * FROM cards WHERE niveau != 0 AND dateDeVue < NOW() ");
-$req = $sth->execute();
-$bleurg = $sth->fetch() ;
-
-/* styles PDOStatement::fetch */
-print("PDO::FETCH_ASSOC: ");
-print("Retourne la ligne suivante en tant qu'un tableau indexé par le nom des colonnes\n<hr />");
-$result = $sth->fetch(PDO::FETCH_ASSOC);
-        
-do{
-	print_r($bleurg);
-	}while( $sth->fetch() )
+        }
 ?>
