9cfecfa2672d679391fc30d0c81fcfe68e1ccd05
moi <moi@moi.moi>
2019-06-09 13:41:59 +0200

    Revert "supprision et renomer 1-dex en index"
    This reverts commit 40e7a00a8dbbc595d9af65ed40db87bef2fe02cf.

---
+++ 1-dex.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once('fonction/racine.php') ;
+
+$ite = $_COOKIE["serveur"] ;
+$racine = $_COOKIE["racine"] ;
+
+echo '<h1>'.$ite.'</h1>' ;
+include_once("$racine/Vue/head.html") ;
+include_once("$racine/Vue/menu.html") ;
+?>
---
+++ 1-index.php
@@ -0,0 +1,8 @@
+<?php
+  include_once('fonction/racine.php') ;
+
+include_once("$racine/Vue/head.html") ;
+
+include_once("$racine/Vue/menu.html") ;
+
+?>
