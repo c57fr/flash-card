c11f940e32f8f50fbcb4e986cac1da5440f29af6
moi <moi@moi.moi>
2019-06-09 14:16:28 +0200

    ajout d'un index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once("fonction/racine.php") ;
+
+$ite = $_COOKIE["serveur"] ;
+$racine = $_COOKIE["racine"] ;
+
+
+include_once("Vue/head.html") ;
+include_once("Vue/menu.html") ;
+include_once("Vue/footer.html") ;
+
+?>
