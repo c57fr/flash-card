32e0006d1627ce66e40cf54afc927153dc9c9f03
moi <moi@moi.moi>
2019-06-09 13:45:07 +0200

    Revert "teste a effacer"
    This reverts commit 3326944b010f9d98ce950ec740c586611b28c402.

--- uuu.php
+++ uuu.php
@@ -3,17 +3,24 @@
         {
 
             include_once("/var/www/html/flash-card/fonction/bdd.php") ;
- 
-
+ /*       
+            $req = $bdd->prepare('SELECT * FROM famille WHERE  famille= famille ) VALUES ( :famille ) ';
+            
+            $req->execute(array(
+            'famille' => $id 
+            ));
+            
+            $donnees = $req->fetch() ;
+            return $donnees ;
 
 $stmt = $dbh->prepare("SELECT * FROM famille where famille = ?");
-/*
-
 $stmt->execute(array( $id ))
 
 $row = $stmt->fetch()
     print_r($row);
+
 */
+
         }
 
 //              $aaa = rechercheFmille(13);
