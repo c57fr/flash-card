cd335153b50a43993cdcf93fc32c37bf05818641
moi <moi@moi.moi>
2019-06-09 13:45:06 +0200

    Revert "teste a effacer"
    This reverts commit 6b97f76b44ec527f33969cb48c45792af13a6204.

--- uuu.php
+++ uuu.php
@@ -7,15 +7,16 @@
 
 
 $stmt = $dbh->prepare("SELECT * FROM famille where famille = ?");
+/*
 
 $stmt->execute(array( $id ))
-/*
+
 $row = $stmt->fetch()
     print_r($row);
 */
         }
 
 //              $aaa = rechercheFmille(13);
-        echo '<hr />oki doki <hr />' ;
+        echo '<hr />ok<hr />' ;
 //        print_r($aaa) ;
 ?>
