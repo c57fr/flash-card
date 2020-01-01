02307b111618309a03ef6b5ec1053d183097dd4b
lansfeust <lansfeust@hotmail.fr>
2020-01-01 12:09:56 +0100

    Passage d'une carte a l'autre grace au javascript opérationnel

--- Vue/listeFlashCard.php
+++ Vue/listeFlashCard.php
@@ -1,3 +1,26 @@
+<style>
+.masquer
+{
+    display : none ;
+}
+#carte_0
+{
+    display : block ;
+}
+</style>
+
+<script>
+    var i = 0 ;
+    function affMasqueFlashCard()
+    {
+//        alert( 'flashCard_'+i );
+        document.getElementById( 'carte_'+i ).style.display = "none" ;
+        i++ ;
+        document.getElementById( 'carte_'+i ).style.display = "block" ;
+    }
+</script>
+
+
 <?php
 //  session_start() ; //EFFACE-MOI
 //  $reussi = $_SESSION['flashcard'] ; //EFFACE-MOI
@@ -7,38 +30,40 @@ foreach ($listeFlashCard as $key => $table) {
 //    print_r( $table ) ;
 ?>
 
-<form method='POST' action target='_blank' >
+<form method='POST' action target='_blank'   class='masquer' id='carte_<?= $i ?>'>
 
-<input type='hidden' name='id' value=' <?= $table['ID'] ?> ' >
-<input type='hidden' name='niveau' value=' <?= $table['niveau'] ?> ' >
+    <input type='hidden' name='id' value=' <?= $table['ID'] ?> ' >
+    <input type='hidden' name='niveau' value=' <?= $table['niveau'] ?> ' >
 
-<section  id='carte_<?= $i ?>' >
-    <h2>ID = <?= $table['ID'] ?> </h2>
-    <h2>famille = <?= $table['famille'] ?> </h2>
-    <article class='question'>
-        <fieldset>
-            <legend>Question</legend>
-            <?= $table['question'] ?>
-        </fieldset>  
-    </article>
+    <section >
+        <h2>ID = <?= $table['ID'] ?> </h2>
+        <h2>famille = <?= $table['famille'] ?> </h2>
+        <article class='question'>
+            <fieldset>
+                <legend>Question</legend>
+                <?= $table['question'] ?>
+            </fieldset>  
+        </article>
 
-    <div class='affReponse' >
-        <p>Affiché la réponse</p>
-    </div>
+        <div class='affReponse' >
+            <p>Affiché la réponse</p>
+        </div>
 
-    <article class='reponse_<?= $i ?>' >
-        <fieldset>   
-            <legend>Réponse</legend>   
-            <?= $table['reponse'] ?>
-        </fieldset>  
+        <article class='reponse_<?= $i ?>' >
+            <fieldset>   
+                <legend>Réponse</legend>   
+                <?= $table['reponse'] ?>
+            </fieldset>  
 
-        <input type="submit" name='reponse' value='juste' >
-        <input type="submit" name='reponse' value='faux' >
-    </article>
-    
-</section>
+            <input type="submit" name='reponse' value='juste' >
+            <input type="submit" name='reponse' value='faux' >
+        </article>
+        
+    </section>
 </form>
+
 <?php
 $i++ ;
 }
 ?>
+<h3 onclick="affMasqueFlashCard( );">Passer a la flash-card suivante</h3>
--- controleur/affichageFlashCard.php
+++ controleur/affichageFlashCard.php
@@ -2,7 +2,8 @@
 
 require_once("$racine/fonction/bdd.php");
 include_once("$racine/fonction/objets/affCard.php") ;
-
+?>
+<?php
 $card = new affCard($bdd) ;
 $listeFlashCard = $card -> reqSelct() ; // EFFACE-MOI
 
